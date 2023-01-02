3aba24bf4a458dd3fc264a82310499c61daf2abf
jimroxodezi <jimroxodezi@gmail.com>
2023-01-02 11:35:19 +0100

    radical changes

--- core-C/strings/arr_strings.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-
-    char stuff[][10] = {"mangoes", "tomatoes", "Melon"};    // wastes space
-    char *stuffs[] = {"mangoes", "tomatoes", "Melon", "Fish"};  // better (array of pointers)
-    return 0;
-}
--- core-C/strings/puts.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    char *str = "Hello, World";
-    puts(str);  // automatically adds a newline
-    printf("%s", str);
-}
